39372675c97e968e6d261073c549106eb88368df
Johan Guse <johanguse@gmail.com>
2024-01-31 10:53:02 -0300

    update playwright baseurl

--- __tests__/e2e/app.spec.ts
+++ __tests__/e2e/app.spec.ts
@@ -1,12 +1,13 @@
 import { expect, test } from '@playwright/test'
 
-const PORT = process.env.PORT || 3000
-const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`
+import { absoluteUrl } from '../../lib/utils'
+
+const BASE_URL = absoluteUrl('')
 
 test('is able to navigate to the home page and find form fields', async ({
   page,
 }) => {
-  await page.goto('http://localhost:3000')
+  await page.goto(BASE_URL)
   await expect(page.locator('input[type="text"]')).toHaveCount(1)
   await expect(page.locator('input[type="tel"]')).toHaveCount(1)
   await expect(page.locator('input[type="email"]')).toHaveCount(1)
