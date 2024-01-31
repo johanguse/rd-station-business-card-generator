9735ace1574dcc8c413162b60973c8dbbe77ab57
Johan Guse <johanguse@gmail.com>
2024-01-31 15:29:32 -0300

    update gh action test

--- __tests__/e2e/app.spec.ts
+++ __tests__/e2e/app.spec.ts
@@ -1,15 +1,16 @@
-import { expect, test } from '@playwright/test'
+import { expect, test } from '@playwright/test';
+import { absoluteUrl } from '../../lib/utils';
 
-import { absoluteUrl } from '../../lib/utils'
+const PORT = process.env.PORT || '3000';
+const BASE_URL = process.env.PLAYWRIGHT_TEST_BASE_URL || absoluteUrl('') || `http://localhost:${PORT}`;
 
-const BASE_URL = absoluteUrl('')
+test('is able to navigate to the home page and find form fields', async ({ page }) => {
+  await page.goto(BASE_URL);
 
-test('is able to navigate to the home page and find form fields', async ({
-  page,
-}) => {
-  await page.goto(BASE_URL)
-  await expect(page.locator('input[type="text"]')).toHaveCount(1)
-  await expect(page.locator('input[type="tel"]')).toHaveCount(1)
-  await expect(page.locator('input[type="email"]')).toHaveCount(1)
-  await expect(page.locator('text=GERAR CARTÃO GRÁTIS')).toBeVisible()
-})
+  await Promise.all([
+    expect(page.locator('input[type="text"]')).toHaveCount(1),
+    expect(page.locator('input[type="tel"]')).toHaveCount(1),
+    expect(page.locator('input[type="email"]')).toHaveCount(1),
+    expect(page.locator('text=GERAR CARTÃO GRÁTIS')).toBeVisible(),
+  ]);
+});
