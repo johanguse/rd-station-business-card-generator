be19f7a1d2facbd626a37922794eb8b522174cf3
Johan Guse <johanguse@gmail.com>
2024-02-01 16:06:36 -0300

    feat: add favicons and manifest file

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -5,13 +5,23 @@ import { siteConfig } from '@/config/site'
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: siteConfig.name,
-    short_name: 'RD Stations',
+    short_name: siteConfig.description,
     description: siteConfig.description,
-    start_url: './',
+    start_url: '/',
     display: 'standalone',
     background_color: '#4839A3',
     theme_color: '#fff',
     icons: [
+      {
+        src: 'assets/favicons/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
+        src: 'assets/favicons/rd-icon.png',
+        sizes: 'any',
+        type: 'image/png',
+      },
       {
         src: 'assets/favicons/favicon-16x16.png',
         sizes: '16x16',
