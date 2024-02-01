6384d811fc578bd4abfc42488b7a865336a84b94
Johan Guse <johanguse@gmail.com>
2024-02-01 16:12:48 -0300

    feat: add favicons and manifest file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -11,6 +11,7 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api'],
     },
+    sitemap: `${siteUrl}/sitemap.xml`,
     host: `${siteUrl}`,
   }
 }
