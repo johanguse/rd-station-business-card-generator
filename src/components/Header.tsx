18fab32034be4bab5a3c84e68cab6641e1dbaca8
Johan Guse <johanguse@gmail.com>
2024-02-02 16:14:10 -0300

    fix: remove legacy objectFit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,14 @@ export default function Header() {
   return (
     <header className="bg-white px-4 py-6">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="">
+        <div className="w-28 sm:w-36">
           <Image
             priority
             src="/assets/images/logo-rd-station-default.svg"
             alt="Logo RD Station"
-            objectFit="contain"
+            layout="responsive"
             width={100}
             height={28}
-            className="sm:w-32"
           />
         </div>
         <p className="font-bold">Gerador de Cartão de Visita</p>
