59abfea61825ea5c7cf43f5014f9f795a51ea169
Johan Guse <johanguse@gmail.com>
2024-02-08 15:43:00 -0300

    feat: create business card component

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from 'next/navigation'
 
 import { useFormLeadStore } from '@/store/form-lead'
 
+import BusinessCard from '@/components/BusinessCard'
 import { Button } from '@/components/Button'
 import { Icons } from '@/components/Icons'
 
@@ -34,21 +35,7 @@ export default function CardPage() {
             >
               Gerar outro cartão
             </Button>
-            <div className="rounded-3xl bg-white px-0 py-8 shadow-md md:px-6">
-              <div className="flex flex-row px-2 xs:px-6 md:px-8">
-                <div className="self-center">
-                  <Icons.rdIcon className="w-16" />
-                </div>
-                <div className="mx-4 flex w-1 bg-primary-superLight md:mx-6">
-                  &nbsp;
-                </div>
-                <div className="grid grid-rows-1 gap-6 py-4 pl-2 text-sm text-black sm:text-xl-sm">
-                  <p>{formData.name}</p>
-                  <p>{formData.phone}</p>
-                  <p>{formData.email}</p>
-                </div>
-              </div>
-            </div>
+            <BusinessCard />
             <Button id="download-card-button" variant={'secondary'} disabled>
               Baixar cartão
             </Button>
---
+++ src/components/BusinessCard.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useFormLeadStore } from '@/store/form-lead'
+
+import { Button } from './Button'
+import { Icons } from './Icons'
+
+export default function BusinessCard() {
+  const formData = useFormLeadStore((state) => state.formData)
+  if (!useFormLeadStore.getState().formData) {
+    return (
+      <div className="mb-6 rounded-3xl bg-white px-0 py-8 shadow-md md:px-6">
+        <p>Preencha seus dados na primeira pagina</p>
+        <Button variant="link" href="/">
+          Ir para primeira pagina
+        </Button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="rounded-3xl bg-white px-0 py-8 shadow-md md:px-6">
+      <div className="flex flex-row px-2 xs:px-6 md:px-8">
+        <div className="self-center">
+          <Icons.rdIcon className="w-16" />
+        </div>
+        <div className="mx-4 flex w-1 bg-primary-superLight md:mx-6">
+          &nbsp;
+        </div>
+        <div className="grid grid-rows-1 gap-6 py-4 pl-2 text-sm text-black sm:text-xl-sm">
+          <p>{formData?.name}</p>
+          <p>{formData?.phone}</p>
+          <p>{formData?.email}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
