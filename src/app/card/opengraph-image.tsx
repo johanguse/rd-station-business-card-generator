d779607fd5b7c165a39406c543ecccbfda5b5b6c
Johan Guse <johanguse@gmail.com>
2024-02-12 14:34:30 -0300

    feat: add dynamic og image to card share

--- src/app/card/opengraph-image.tsx
+++ src/app/card/opengraph-image.tsx
@@ -24,18 +24,75 @@ export default async function Image() {
       // ImageResponse JSX element
       <div
         style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
           display: 'flex',
+          height: '100%',
+          width: '100%',
           alignItems: 'center',
           justifyContent: 'center',
+          letterSpacing: '-.02em',
+          fontWeight: 700,
+          background: 'white',
         }}
       >
-        About Acme
+        <div
+          style={{
+            left: 42,
+            top: 160,
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <span
+            style={{
+              marginLeft: 8,
+              fontSize: 20,
+            }}
+          >
+            <svg height={90} viewBox="0 0 150 128">
+              <g clipPath="url(#a)">
+                <path
+                  fill="#806EFF"
+                  d="m111.571 48.026-34.66 55.935L88.691 128l34.66-55.935-11.78-24.039ZM34.66 0 0 55.934l11.78 24.04 34.66-55.935L34.66 0ZM84.921 48.026l14.87-23.987-9.53-19.378L88.012 0 58.22 48.026l-34.66 55.935L35.34 128 70 72.066l-3.874-7.961-7.906-16.079H84.92Z"
+                />
+                <path
+                  fill="#000"
+                  d="m150 72.065-11.78-24.039h-26.649l11.78 24.04L88.691 128h26.649L150 72.065ZM73.09 24.04 61.31 0H34.66l11.78 24.04-34.66 55.934h26.65l34.66-55.935Z"
+                />
+                <path
+                  fill="#000"
+                  d="m96.702 72.066 29.738-48.027L114.66 0H88.01l11.78 24.04-14.869 23.986H58.22L70 72.066 35.34 128h26.702L91.78 79.974l4.922-7.909Z"
+                />
+              </g>
+              <defs>
+                <clipPath id="a">
+                  <path fill="#fff" d="M0 0h150v128H0z" />
+                </clipPath>
+              </defs>
+            </svg>
+          </span>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '0',
+            margin: '0',
+            fontSize: 30,
+            width: 'auto',
+            maxWidth: 800,
+            textAlign: 'left',
+            color: '#100C27',
+            lineHeight: 1.4,
+          }}
+        >
+          <p>Johan Guse</p>
+          <p>(47) 9999999</p>
+          <p>johanguse@gmail.com</p>
+        </div>
       </div>
     ),
+
     // ImageResponse options
     {
       // For convenience, we can re-use the exported opengraph-image
